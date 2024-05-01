54c453c75663bb6967442b58d7498114bb852d5c
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-05-01 10:06:55 +0530

    create new project

---
+++ TechHeaven/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
