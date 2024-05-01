4e1b88cc1d22b847aa950b3b64aa9463f22cf762
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-05-01 12:03:46 +0530

    edit

--- TechHeaven/src/Components/Navbar/Navbar.jsx
+++ TechHeaven/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <img src={assets.logo} alt="" className="logo" />
+        <ul className="navbar-menu">
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Mobile-app</li>
+            <li>contact us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="" />
+            <div className="navbar-search_icon">
+                <img src={assets.busket} alt="" />
+                <div className="dot"></div>
+            </div>
+            <button>Sign in</button>
+        </div>
+        </div>
   )
 }
 
---
+++ TechHeaven/src/assets/assets.js
@@ -0,0 +1,8 @@
+import logo from './Tech.png'
+import search_icon from './magnifying-glass.png'
+import busket from './shopping-online.png'
+export const assets={
+    logo,
+    search_icon,
+    busket,
+}
