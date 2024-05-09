27aa85ee2afd5a2c51bacf5ae95384860f31b776
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-05-09 22:48:35 +0530

    make folder frontend

--- TechHeaven/Frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ TechHeaven/Frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -13,7 +13,6 @@ const Loginpopup = ({setShowLogin}) => {
             </div>
             <div className="login-popup-inputs">
                 {currentState==="Login"?<></>:<input type="text"placeholder='Your name' required />}
-                
                 <input type="email"placeholder='Your email' required />
                 <input type="password"placeholder='Your password' required />
             </div>
