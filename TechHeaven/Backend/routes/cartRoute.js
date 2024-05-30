393d507b42f4e60bf421e0e99d643b63a5fe650d
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-05-30 21:07:06 +0530

    fix error

--- TechHeaven/Backend/routes/cartRoute.js
+++ TechHeaven/Backend/routes/cartRoute.js
@@ -6,6 +6,6 @@ const cartRouter = express.Router();
 
 cartRouter.post('/add',authMiddleware,addToCart);
 cartRouter.post('/remove',authMiddleware,removeFromCart);
-cartRouter.get('/get',authMiddleware,getCart);
+cartRouter.post('/get',authMiddleware,getCart);
 
 export default cartRouter;
