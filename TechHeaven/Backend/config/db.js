c455cb9e76f30674cb486d009db6c72930211257
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-09-09 17:17:32 +0530

    small change

--- TechHeaven/Backend/config/db.js
+++ TechHeaven/Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const connectDB =async()=>{
-    await mongoose.connect('mongodb+srv://udayangakasun696:GDGvQ6vTbTFOWQcX@cluster0.vn1mgz6.mongodb.net/e-commerce').then(()=>{console.log('DB connected')});
+    await mongoose.connect('mongodb+srv://udayangakasun696:p58R_2_F2ytXB_w@cluster0.vn1mgz6.mongodb.net/e-commerce').then(()=>{console.log('DB connected')});
 }
