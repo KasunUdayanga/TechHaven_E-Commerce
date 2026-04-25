93e6ad3d344ad8041855347f6cdf71c97e4867be
Kasun_Udayanga <udayangakasun696@gmail.com>
2026-04-25 11:32:00 +0530

    fix: update API base URL to use the production endpoint

--- TechHeaven/admin/src/App.jsx
+++ TechHeaven/admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url="http://localhost:4000";
+  const url="https://techhaven-e-commerce.onrender.com";
   return (
     <div>
       <ToastContainer/>
