0ba2394858c5a320e9849bab237b227eef31492e
Kasun_Udayanga <udayangakasun696@gmail.com>
2026-04-25 09:54:04 +0530

    fix: update API endpoint URL to use the correct port

--- TechHeaven/Frontend/src/context/StoreContext.jsx
+++ TechHeaven/Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems,setCartItems] = useState({});
-  const url="http://localhost:4000";
+  const url="http://localhost:10000";
   const [token,setToken] = useState("")
   const[item_list,setItem_list] = useState([])
 
