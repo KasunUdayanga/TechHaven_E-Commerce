781a645f9bdfe281f501cda2edd28f46930cc6a0
Kasun_Udayanga <udayangakasun696@gmail.com>
2026-04-25 08:24:46 +0530

    Add .gitignore file and update MongoDB connection string in db.js

--- TechHeaven/Backend/config/db.js
+++ TechHeaven/Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const connectDB =async()=>{
-    await mongoose.connect('mongodb+srv://udayangakasun696:p58R_2_F2ytXB_w@cluster0.vn1mgz6.mongodb.net/e-commerce').then(()=>{console.log('DB connected')});
+    await mongoose.connect('mongodb+srv://udayangakasun696:t4pTAJdmVaGCaFj0@cluster0.vn1mgz6.mongodb.net/e-commerce').then(()=>{console.log('DB connected')});
 }
